69749446912b0717b756690e45969f3e40bc2cf6
Richard Freeland <richard_freeland@hotmail.com>
2022-03-03 17:58:49 +0000

    initial local commit

---
+++ customer/src/main/java/org/richard/customer/CustomerService.java
@@ -0,0 +1,21 @@
+package org.richard.customer;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+
+	public void registerCustomer(CustomerRegistrationRequest customerRequest) {
+		Customer customer = Customer.builder()
+				.firstname(customerRequest.getFirstname())
+				.lastname(customerRequest.getLastname())
+				.email(customerRequest.getEmail())
+				.build();
+		
+		// TODO: check email valid
+		// TODO: check email not taken
+		// TODO: store customer in database
+	}
+
+}
